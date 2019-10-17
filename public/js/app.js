18d12cd38e3aea7d9d3dd9caa5c7c852a0558387
cleiton080 <cleitonpessoa53@hotmail.com>
2019-10-17 17:58:35 -0300

    default layout

---
+++ public/js/app.js
@@ -0,0 +1,44 @@
+/**
+ * Modal
+*/
+function Modal(modal) {
+    this.fadeIn = modal.fadeIn;
+    this.fadeOut = modal.fadeOut;
+}
+  
+Modal.prototype.open = function(id) {
+    let modal = document.querySelector(`#${id}`);
+
+    modal.classList.contains('modal-show') || modal.classList.add('modal-show');
+    setTimeout(function(){ modal.classList.add('modal-fadeIn'); }, this.fadeIn);
+}
+  
+Modal.prototype.close = function() {
+    let modal = document.querySelector('.modal-show');
+
+    modal.classList.add('modal-fadeOut');
+    setTimeout(function(){
+        modal.classList.remove('modal-fadeIn');
+        modal.classList.remove('modal-fadeOut');
+        modal.classList.remove('modal-show');
+    }, this.fadeOut)
+}
+  
+/**
+ * Nav
+*/
+function Nav(o) {
+    this.id = o;
+}
+  
+Nav.prototype.click = function() {
+    let nav = document.querySelector(this.id);
+
+    if(nav.classList.contains('nav-show')) {
+        nav.classList.remove('nav-show');
+        nav.classList.add('nav-off');
+    } else {
+        nav.classList.remove('nav-off');
+        nav.classList.add('nav-show');
+    }
+}
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <!-- Metas -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>{{ config('app.name') }}</title>
+
+    <!-- Links / Styles -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css" integrity="sha256-46qynGAkLSFpVbEBog43gvNhfrOj+BmwXdxFgVK/Kvc=" crossorigin="anonymous" />
+    <style media="screen">
+
+      .fa-star { color: #FFC300; }
+
+      .fa-project-diagram { color: #46C5D9; }
+
+      .fa-users { color: #FFEAC4; }
+
+      .card { flex-grow: 2; }
+
+    </style>
+    <!-- .Links / Styles -->
+  </head>
+  <body>
+    <main>
+
+      <div class="d-flex">
+
+        <!-- Nav -->
+        <aside class="nav nav-show">
+          <div class="nav-head">
+            <div class="nav-brand">
+              <h4 class="title">My kanban</h4>
+            </div>
+            <div class="nav-user">
+              <img src="http://www.urhobosocialclublagos.com/wp-content/uploads/2017/07/default-avatar-ginger-guy.png" class="user-profile">
+              <p>{{ Auth::user()->name }} <br> {{ Auth::user()->email }} </p>
+            </div>
+          </div>
+
+          <ul class="nav-body">
+            <li class="nav-item">
+              <i class="fas fa-star"></i> &nbsp;Favorites
+            </li>
+            <li class="nav-item">
+              <i class="fas fa-project-diagram"></i> &nbsp;Projects
+            </li>
+            <li class="nav-item">
+              <i class="fas fa-users"></i> &nbsp;Teams
+            </li>
+            <li class="nav-item" onclick="modal.open('github')">
+              <i class="fab fa-github"></i> &nbsp;Github
+            </li>
+          </ul>
+
+          <div class="nav-swipe">
+            <i class="fas fa-angle-right"></i>
+          </div>
+        </aside>
+        <!-- .Nav -->
+
+        <div class="row">
+
+            <!-- Navbar -->
+            <nav class="navbar">
+                <ul class="navbar-menu">
+                <li class="navbar-item dropdown">
+                    <a href="#" class="navbar-link"> {{ Auth::user()->name }} &nbsp;<i class="fas fa-angle-down"></i></a>
+                    <ul class="dropdown-menu" style="top: 3.6em; right: 0;">
+                    <li>Settings</li>
+                    <li>Help</li>
+                    <li>Logout</li>
+                    </ul>
+                </li>
+                </ul>
+            </nav>
+            <!-- .Navbar -->
+
+          <section class="container">
+            @yield('content')
+          </section>
+
+        </div>
+      </div>
+
+    </main>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" charset="utf-8"></script>
+    <script>
+
+        // Show / Hidden side nav
+        const nav = new Nav('.nav');
+        document.querySelector('.nav-swipe').addEventListener('click', function() { nav.click() });
+
+    </script>
+
+    @yield('script')
+    <!-- .Scripts -->
+
+  </body>
+</html>
