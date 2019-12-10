fa598db31050401fd560f7f4e463030b6a95dd85
cleiton080 <cleitonpessoa53@hotmail.com>
2019-12-10 00:50:20 -0300

    js updated

--- public/js/app.js
+++ public/js/app.js
@@ -1,24 +1,18 @@
-// function bindId(id, match) {
-//     let field = document.querySelector(match).value = id;
-
-//     return field ? true : false
-// }
-  
 /**
  * Nav
-*/
+ */
 function Nav(o) {
     this.id = o;
 }
-  
+
 Nav.prototype.click = function() {
     let nav = document.querySelector(this.id);
 
-    if(nav.classList.contains('nav-show')) {
-        nav.classList.remove('nav-show');
-        nav.classList.add('nav-off');
+    if (nav.classList.contains("nav-show")) {
+        nav.classList.remove("nav-show");
+        nav.classList.add("nav-off");
     } else {
-        nav.classList.remove('nav-off');
-        nav.classList.add('nav-show');
+        nav.classList.remove("nav-off");
+        nav.classList.add("nav-show");
     }
-}
+};
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,101 +11,85 @@
 
 @section('content')
 
-    <h3>
-        <i class="fas fa-project-diagram"></i> &nbsp;PROJETOS
-    </h3>
+<h3>
+    <i class="fas fa-project-diagram"></i> &nbsp;PROJETOS
+</h3>
 
-    <div class="divide"></div>
+<div class="divide"></div>
 
-    <div class="d-flex row-wrap" id="projects">
+<div class="d-flex row-wrap" id="projects">
 
-        <!-- All projects -->
-        @foreach($allProjects as $project)
-            <a href="{{ route('project.board', ['id' => $project->id]) }}" class="btn btn-xl btn-dark" data-id="{{ $project->id }}">
-                {{ $project->name }}
-            </a>
-        @endforeach
-        <!-- .All projects -->
+    <!-- All projects -->
+    @foreach($allProjects as $project)
+    <a href="{{ route('project.board', ['id' => $project->id]) }}" class="btn btn-xl btn-dark" data-id="{{ $project->id }}">
+        {{ $project->name }}
+    </a>
+    @endforeach
+    <!-- .All projects -->
 
-        <!-- Contextmenu -->
-        <div class="contextmenu">
-            <ul>
-                <li style="border-bottom: 1px solid #363b41;">Abrir</li>
-                <li data-target="#delete-project">Deletar</li>
-            </ul>
+    <!-- Contextmenu -->
+    <div class="contextmenu">
+        <ul>
+            <li style="border-bottom: 1px solid #363b41;">Abrir</li>
+            <li data-target="#delete-project">Deletar</li>
+        </ul>
+    </div>
+    <!-- .Contextmenu -->
+
+</div>
+
+<hr>
+
+<!-- New project button -->
+<div class="">
+    <button type="button" class="btn btn-block btn-xl btn-dark" data-target="#add-project">
+        <i class="fas fa-plus"></i> &nbsp;NOVO PROJETO
+    </button>
+</div>
+<!-- .New project button -->
+
+@component('components.modal', ['id' => 'add-project', 'title' => 'NOVO PROJETO'])
+<form method="post" action="{{ route('project.create') }}">
+    <div class="modal-body">
+        @csrf
+        <div class="form-group">
+            <input type="text" name="name" class="input-control" placeholder="nome do projeto">
         </div>
-        <!-- .Contextmenu -->
-    
+        <div class="form-group">
+            <textarea class="input-control" name="description" placeholder="digite uma breve descrição sobre o projeto"></textarea>
+        </div>
     </div>
-
-    <hr>
-
-    <!-- New project button -->
-    <div class="">
-        <button type="button" class="btn btn-block btn-xl btn-dark" data-target="#add-project">
-            <i class="fas fa-plus"></i> &nbsp;NOVO PROJETO
-        </button>
+    <div class="modal-footer">
+        <button type="button" class="btn btn-std btn-gray" data-dismiss="#add-project">Cancelar</button>
+        <button type="submit" class="btn btn-std btn-blue">Salvar</button>
     </div>
-    <!-- .New project button -->
+</form>
+@endcomponent
 
-    @component('components.modal', ['id' => 'add-project', 'title' => 'NOVO PROJETO'])
-        <form method="post" action="{{ route('project.create') }}">
-            <div class="modal-body">
-                @csrf
-                <div class="form-group">
-                    <input type="text" name="name" class="input-control" placeholder="nome do projeto">
-                </div>
-                <div class="form-group">
-                    <textarea class="input-control" name="description" placeholder="digite uma breve descrição sobre o projeto"></textarea>
-                </div>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-std btn-gray" data-dismiss="#add-project">Cancelar</button>
-                <button type="submit" class="btn btn-std btn-blue">Salvar</button>
-            </div>
-        </form>
-    @endcomponent
-
-    @component('components.modal', ['id' => 'delete-project', 'title' => 'DELETAR PROJETO'])
-        <form action="{{ route('project.delete') }}" method="post">
-            <div class="modal-body">
-                <p>
-                    <strong>Você realmente deseja deletar o projeto selecionado?</strong><br><br>
-                    <i>O item irá ser deletado imediatamente, a ação não poderá ser desfeita.</i>
-                </p>
-                @method('delete')
-                @csrf
-                <input type="hidden" name="id-project">
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-std btn-blue" data-dismiss="#delete-project">Cancelar</button>
-                <button type="submit" class="btn btn-std btn-gray">Deletar</button>
-            </div>
-        </form>
-    @endcomponent
+@component('components.modal', ['id' => 'delete-project', 'title' => 'DELETAR PROJETO'])
+<form action="{{ route('project.delete') }}" method="post">
+    <div class="modal-body">
+        <p>
+            <strong>Você realmente deseja deletar o projeto selecionado?</strong><br><br>
+            <i>O item irá ser deletado imediatamente, a ação não poderá ser desfeita.</i>
+        </p>
+        @method('delete')
+        @csrf
+        <input type="hidden" name="id-project">
+    </div>
+    <div class="modal-footer">
+        <button type="button" class="btn btn-std btn-blue" data-dismiss="#delete-project">Cancelar</button>
+        <button type="submit" class="btn btn-std btn-gray">Deletar</button>
+    </div>
+</form>
+@endcomponent
 
 @stop
 
 @section('script')
-    <script type="module">
-        import Modal from '/js/Plugins/Modal.js';
-        import ContextMenu from '/js/Plugins/ContextMenu.js';
-        
-        // Contextmenus
-        const contextmenu = new ContextMenu({
-            wrapper: document.querySelectorAll('#projects a'),
-            menu: document.querySelector('.contextmenu')
-        });
-
-        // Modals
-        const addModal = new Modal({ modal: document.getElementById('add-project') });
-        const deleteModal = new Modal({
-            modal: document.getElementById('delete-project'),
-            opening: function() {
-                this.querySelector('input[name=id-project]').value = contextmenu.lastEventElement.getAttribute('data-id');
-            }
-        });
-
-
-    </script>
+<script src="{{ asset('js/components/modal.js') }}"></script>
+<script>
+    modal(document.getElementById("add-project")).init();
+    modal(document.getElementById("delete-project")).init();
+</script>
 @stop
